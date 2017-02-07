6ec0b785781ed69b1b788b8a48d31d3a1cbc992b
Matthew Bendall <bendall@gwu.edu>
2017-02-07 14:31:57 -0500

    fixed bug in refine_assembly

--- haphpipe/stages/refine_assembly.py
+++ haphpipe/stages/refine_assembly.py
@@ -115,9 +115,9 @@ def refine_assembly(fq1=None, fq2=None, fqU=None, assembly_fa=None, outdir='.',
     cmd5 += ['-S', os.path.join(tempdir, 'tmp.sam'), ]
     cmd5 += ['2>&1', '|', 'tee', out_bt2, ]
     cmd6a = ['samtools', 'view', '-bS', os.path.join(tempdir, 'tmp.sam'), '>', os.path.join(tempdir, 'unsorted.bam'),]
-    cmd6b = ['samtools', 'sort', os.path.join(tempdir, 'unsorted.bam'), os.path.join(tempdir, 'all'),]
-    cmd6c = ['samtools', 'index',  os.path.join(tempdir, 'all.bam'),]
-    cmd7 = ['rm', os.path.join(tempdir, 'tmp.sam'), os.path.join(tempdir, 'unsorted.bam'), ]
+    cmd6b = ['samtools', 'sort', os.path.join(tempdir, 'unsorted.bam'), os.path.join(tempdir, 'aligned'),]
+    cmd6c = ['samtools', 'index',  os.path.join(tempdir, 'aligned.bam'),]
+    cmd7 = ['rm', '-f', os.path.join(tempdir, 'tmp.sam'), os.path.join(tempdir, 'unsorted.bam'), ]
     command_runner([cmd5,cmd6a,cmd6b,cmd6c,cmd7], 'refine_assembly:align', debug)
     
     # MarkDuplicates
